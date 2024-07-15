14fac4e61c5e210c85419f0cd40502615b8d6380
pamatya <hydroweb.pranujamatya@gmail.com>
2024-07-15 07:21:34 +0200

    Broke the index_list function into two functions.

--- src/index.c
+++ src/index.c
@@ -6,7 +6,7 @@
 /*   By: pamatya <pamatya@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 23:24:28 by pamatya           #+#    #+#             */
-/*   Updated: 2024/07/15 06:48:02 by pamatya          ###   ########.fr       */
+/*   Updated: 2024/07/15 07:20:48 by pamatya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	index_list(t_stack **root, int *minmax, int list_size);
 // void	index_list(t_stack **root, int *minmax, int list_size)
 // {
 // 	t_stack	*cur;
-// 	t_stack	*nxt_min;
+// 	t_stack	*nxtmin_node;
 // 	int		new_max;
 // 	int		index;
 // 	int		curr_min;
@@ -28,61 +28,70 @@ void	index_list(t_stack **root, int *minmax, int list_size);
 // 	while (index < list_size)
 // 	{
 // 		cur = *root;
-// 		nxt_min = *root;
+// 		nxtmin_node = *root;
 // 		new_max = minmax[1];
 // 		while (cur)
 // 		{
 // 			if (index == 0 && cur->num == minmax[0])
-// 				nxt_min = cur;
+// 				nxtmin_node = cur;
 // 			else if (index > 0 && cur->num == (curr_min + 1))
 // 			{
-// 				nxt_min = cur;
+// 				nxtmin_node = cur;
 // 				break ;
 // 			}
 // 			else if (index > 0 && cur->num > curr_min && cur->num <= new_max)
 // 			{
-// 				nxt_min = cur;
+// 				nxtmin_node = cur;
 // 				new_max = cur->num;
 // 			}
 // 			cur = cur->next;
 // 		}
-// 		curr_min = nxt_min->num;
-// 		nxt_min->index = index++;
+// 		curr_min = nxtmin_node->num;
+// 		nxtmin_node->index = index++;
 // 	}
 // }
+
+
+static t_stack	*get_nxtmin_node(t_stack *cur, int curr_min, int new_max, int index)
+{
+	t_stack	*nxtmin_node;
+	
+	while(cur)
+	{
+		if (index == 0 && cur->num == curr_min)
+			nxtmin_node = cur;
+		else if (index > 0 && cur->num == (curr_min + 1))
+		{
+			nxtmin_node = cur;
+			break ;
+		}
+		else if (index > 0 && cur->num > curr_min && cur->num <= new_max)
+		{
+			nxtmin_node = cur;
+			new_max = cur->num;
+		}
+		cur = cur->next;
+	}
+	return (nxtmin_node);
+}
 
 // Function to index the list from 0 to n by comparing each node with the smallest current node in the list
 void	index_list(t_stack **root, int *minmax, int list_size)
 {
 	t_stack	*cur;
-	t_stack	*nxt_min;
-	int		new_max;
+	t_stack	*nxtmin_node;
 	int		index;
 	int		curr_min;
+	int		new_max;
 
 	index = 0;
+	curr_min = minmax[0];
 	while (index < list_size)
 	{
 		cur = *root;
-		nxt_min = *root;
 		new_max = minmax[1];
-		while (cur)
-		{
-			if (index == 0 && cur->num == minmax[0])
-				nxt_min = cur;
-			else if (index > 0 && cur->num == (curr_min + 1))
-			{
-				nxt_min = cur;
-				break ;
-			}
-			else if (index > 0 && cur->num > curr_min && cur->num <= new_max)
-			{
-				nxt_min = cur;
-				new_max = cur->num;
-			}
-			cur = cur->next;
-		}
-		curr_min = nxt_min->num;
-		nxt_min->index = index++;
+		nxtmin_node = get_nxtmin_node(cur, curr_min, new_max, index);
+		curr_min = nxtmin_node->num;
+		nxtmin_node->index = index++;
 	}
 }
